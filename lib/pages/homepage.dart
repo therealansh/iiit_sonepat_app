7eaace41e925fd7030f9790dd85c11fd2cc7e080
pulkit0795 <pulkit0795@yahoo.com>
2020-06-28 02:16:02 +0530

    Icons are made scrollable vertically as well as horizontally.

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:iiit_sonepat_stable/widgets/homescreen_buttons.dart';
 
 class HomePage extends StatelessWidget {
-  
   AppBar myAppBar() {
     return AppBar(
       elevation: 10,
@@ -15,15 +14,16 @@ class HomePage extends StatelessWidget {
               Padding(
                 padding: EdgeInsets.all(5),
                 child: IconButton(
-                  onPressed: () {},
-                  highlightColor: Color.fromRGBO(247, 200, 42, 1),
-                  splashColor: Color.fromRGBO(247, 200, 42, 1),
-                  icon: Icon(
-                    Icons.menu, color: Color.fromRGBO(199, 199, 199, 1),)
-                ),
+                    onPressed: () {},
+                    highlightColor: Color.fromRGBO(247, 200, 42, 1),
+                    splashColor: Color.fromRGBO(247, 200, 42, 1),
+                    icon: Icon(
+                      Icons.menu,
+                      color: Color.fromRGBO(199, 199, 199, 1),
+                    )),
               ),
               Text('Home',
-                style: TextStyle(color: Color.fromRGBO(199, 199, 199, 1))),
+                  style: TextStyle(color: Color.fromRGBO(199, 199, 199, 1))),
             ],
           ),
           IconButton(
@@ -36,83 +36,101 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
-  
+
   @override
   Widget build(BuildContext context) {
     final String uName = "Student Test"; //User Name Goes Here
-    return  Scaffold(
-        appBar: myAppBar(),
-        backgroundColor: Color.fromRGBO(34, 36, 38, 1),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Icon(
-                Icons.account_circle,
-                size: 150,
+    return Scaffold(
+      appBar: myAppBar(),
+      backgroundColor: Color.fromRGBO(34, 36, 38, 1),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Icon(
+              Icons.account_circle,
+              size: 150,
+              color: Color.fromRGBO(199, 199, 199, 1),
+            ),
+            Text(
+              uName,
+              style: TextStyle(
+                fontSize: 36,
                 color: Color.fromRGBO(199, 199, 199, 1),
               ),
-              
-              Text(uName, style: TextStyle(fontSize: 36, color: Color.fromRGBO(199, 199, 199, 1),),),
-              ListView(
-                shrinkWrap: true,
-                scrollDirection: Axis.vertical,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      HomeScreenButton(
-                        icon: Icons.account_circle,
-                        text: 'My Profile',
+            ),
+            Expanded(
+              child: Center(
+                child: ListView(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.vertical,
+                  children: <Widget>[
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          HomeScreenButton(
+                            icon: Icons.account_circle,
+                            text: 'My Profile',
+                          ),
+                          HomeScreenButton(
+                            icon: Icons.art_track,
+                            text: 'Attendance',
+                          ),
+                          HomeScreenButton(
+                            icon: Icons.local_library,
+                            text: 'Homework',
+                          ),
+                        ],
                       ),
-                      HomeScreenButton(
-                        icon: Icons.art_track,
-                        text: 'Attendance',
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          HomeScreenButton(
+                            icon: Icons.book,
+                            text: 'Circulars',
+                          ),
+                          HomeScreenButton(
+                            icon: Icons.insert_invitation,
+                            text: 'Time Table',
+                          ),
+                          HomeScreenButton(
+                            icon: Icons.calendar_today,
+                            text: 'Calendar',
+                          ),
+                        ],
                       ),
-                      HomeScreenButton(
-                        icon: Icons.local_library,
-                        text: 'Homework',
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          HomeScreenButton(
+                            icon: Icons.perm_media,
+                            text: 'Gallery',
+                          ),
+                          HomeScreenButton(
+                            icon: Icons.people,
+                            text: 'Staff Details',
+                          ),
+                          HomeScreenButton(
+                            icon: Icons.view_list,
+                            text: 'Results',
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      HomeScreenButton(
-                        icon: Icons.book,
-                        text: 'Circulars',
-                      ),
-                      HomeScreenButton(
-                        icon: Icons.insert_invitation,
-                        text: 'Time Table',
-                      ),
-                      HomeScreenButton(
-                        icon: Icons.calendar_today,
-                        text: 'Calendar',
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      HomeScreenButton(
-                        icon: Icons.perm_media,
-                        text: 'Gallery',
-                      ),
-                      HomeScreenButton(
-                        icon: Icons.people,
-                        text: 'Staff Details',
-                      ),
-                      HomeScreenButton(
-                        icon: Icons.view_list,
-                        text: 'Results',
-                      ),
-                    ],
-                  ),
-                ],
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ),
-          ),
-        );
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
