a2cc22d82795f28a4f57f738c1fd833ab585ca30
Jassi <jaswinder.dandyan2000@gmail.com>
2020-06-28 12:36:08 +0530

    Director msg

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'pages/splash.dart';
-import 'pages/directormsg.dart';
+import 'pages/splash.dart';
 void main() {
   runApp(MyApp());
 }
@@ -10,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DirectorMsg(),
+      home: SplashPage(),
     );
   }
 }
