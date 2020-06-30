55418b6a32baea4a27cb69e0770ed1dd9d387ae6
Jassi <jaswinder.dandyan2000@gmail.com>
2020-06-30 23:11:28 +0530

    About Page Created

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
 
-class About extends StatefulWidget{
+class About extends StatefulWidget {
   _About createState() => _About();
 }
 
-class _About extends State<About>{
-  Widget build(context){
+class _About extends State<About> {
+  @override
+  Widget build(BuildContext context) {
+    String about =
+        "Indian Institute of Information Technology, Sonepat (IIIT, Sonepat) is one of the Indian Institutes of Information Technology located at Sonepat, Haryana. The academic session of IIIT Sonepat started from its mentor Institute National Institute of Technology, Kurukshetra from year 2014. IIIT Sonepat is a joint venture of the Ministry of Human Resource Development, Government of India, the Govt. of Haryana, with Industries in Public-Private Partnership model and has been declared as the Institute of national importance.Currently we are operating in the temporary campus at IIT Delhi Technopark Sonepat. The permanent campus of the Institute is to come up on a 50 acre piece of land awarded by the State Government in village Kilorad of distt. Sonepat. The branches available now in college are CSE ,IT ,ECE.  ";
     return Scaffold(
+      backgroundColor: Color(0xFF13191B),
       body: Center(
-        child: Text("About"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Image.network(
+              'https://sonepat.iiitiansnetwork.com/img/campus.webp',
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
+              child: Text(
+                about,
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                  color: Color.fromRGBO(250, 250, 250, 1),
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            Image.asset(
+              'assets/logo.png',
+              width: 50,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:iiit_sonepat_stable/pages/backdrop.dart';
-import 'package:iiit_sonepat_stable/pages/homepage.dart';
-
 final FirebaseAuth auth = FirebaseAuth.instance;
 
 class SplashPage extends StatefulWidget{
