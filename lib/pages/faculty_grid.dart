738cc53c40e276fb4369611050319c2af138550c
pulkit0795 <pulkit0795@yahoo.com>
2020-07-01 15:14:06 +0530

    Made faculty gridview padding unclickable.

--- lib/pages/faculty_grid.dart
+++ lib/pages/faculty_grid.dart
@@ -10,7 +10,7 @@ AppBar myAppBar() {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
         Text('Faculty',
-          style: TextStyle(color: Color.fromRGBO(199, 199, 199, 1))),
+            style: TextStyle(color: Color.fromRGBO(199, 199, 199, 1))),
         IconButton(
           icon: Icon(Icons.search),
           highlightColor: Color.fromRGBO(247, 200, 42, 1),
@@ -22,7 +22,6 @@ AppBar myAppBar() {
   );
 }
 
-
 class FacultyGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -32,70 +31,61 @@ class FacultyGrid extends StatelessWidget {
         color: Color(0xFF13191B),
         child: GridView.count(
           crossAxisCount: 2,
+//          padding: EdgeInsets.all(3.0),
+          mainAxisSpacing: 8.0,
+          crossAxisSpacing: 8.0,
           childAspectRatio: 1 / 1.1,
           children: <Widget>[
             Builder(
-              builder: (BuildContext context){
+              builder: (BuildContext context) {
                 return InkWell(
-                  onTap: (){
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => mohammadD())
-                    );
-                  },
-                  child: ProfCard(
-                    name: 'Dr. Mohammad Najmud Doja',
-                    image: 'assets/images/doja.jpg',
-                  )
-                );
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => mohammadD()));
+                    },
+                    child: ProfCard(
+                      name: 'Dr. Mohammad Najmud Doja',
+                      image: 'assets/images/doja.jpg',
+                    ));
               },
             ),
             Builder(
-              builder: (BuildContext context){
+              builder: (BuildContext context) {
                 return InkWell(
-                  onTap: (){
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => diddiK())
-                    );
-                  },
-                  child: ProfCard(
-                    name: 'Dr. Diddy Kumara Swamy',
-                    image: 'assets/images/diddi.jpg',
-                  )
-                );
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => diddiK()));
+                    },
+                    child: ProfCard(
+                      name: 'Dr. Diddy Kumara Swamy',
+                      image: 'assets/images/diddi.jpg',
+                    ));
               },
             ),
             Builder(
-              builder: (BuildContext context){
+              builder: (BuildContext context) {
                 return InkWell(
-                  onTap: (){
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => rajivV())
-                    );
-                  },
-                  child: ProfCard(
-                    name: 'Dr. Rajiv Verma',
-                    image: 'assets/images/rajiv.jpg',
-                  )
-                );
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => rajivV()));
+                    },
+                    child: ProfCard(
+                      name: 'Dr. Rajiv Verma',
+                      image: 'assets/images/rajiv.jpg',
+                    ));
               },
             ),
             Builder(
-              builder: (BuildContext context){
+              builder: (BuildContext context) {
                 return InkWell(
-                  onTap: (){
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => mukeshM())
-                    );
-                  },
-                  child: ProfCard(
-                    name: 'Dr. Mukesh Mann',
-                    image: 'assets/images/mann.jpg',
-                  )
-                );
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => mukeshM()));
+                    },
+                    child: ProfCard(
+                      name: 'Dr. Mukesh Mann',
+                      image: 'assets/images/mann.jpg',
+                    ));
               },
             ),
           ],
--- lib/widgets/prof_card.dart
+++ lib/widgets/prof_card.dart
@@ -15,33 +15,29 @@ class ProfCard extends StatelessWidget {
           height: 500,
           child: Center(
             child: Column(
-                children: <Widget> [
-                  Hero(
-                    tag: name,
-                    child: ClipRRect(
+              children: <Widget>[
+                Hero(
+                  tag: name,
+                  child: ClipRRect(
                       borderRadius: BorderRadius.all(Radius.circular(10)),
                       child: Container(
-                        height: 150,
-                        width: 150,
-                        child: Image.asset(image)
-                      )
-                    ),
-                  ), //TODO
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      name,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Color.fromRGBO(199, 199, 199, 1),
-                      ),
+                          height: 150, width: 150, child: Image.asset(image))),
+                ), //TODO
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    name,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Color.fromRGBO(199, 199, 199, 1),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
+            ),
           ),
-          ),
+        ),
       ),
     );
   }
