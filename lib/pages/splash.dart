e5388ff7b429ed1e60e65994c3ce6e3697de5c50
DaNnyKg07 <parasyadavnirban999@gmail.com>
2021-02-11 21:28:47 +0530

    show a spinner while user waits during login

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:iiit_sonepat_stable/models/users.dart';
 import 'package:iiit_sonepat_stable/pages/backdrop.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 final FirebaseAuth auth = FirebaseAuth.instance;
 final userRef = Firestore.instance.collection('users');
@@ -16,7 +17,7 @@ class _SplashPage extends State<SplashPage> {
   final GlobalKey<FormState> _loginKey = GlobalKey<FormState>();
   TextEditingController email = TextEditingController();
   TextEditingController pass = TextEditingController();
-
+  bool showSpinner = false;
   @override
   void initState() {
     super.initState();
@@ -106,91 +107,100 @@ class _SplashPage extends State<SplashPage> {
   Scaffold buildUnAuthPage() {
     return Scaffold(
       backgroundColor: Color(0xFF13191b),
-      body: Container(
-        child: ListView(
-          children: <Widget>[
-            Container(
-                constraints: BoxConstraints.expand(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height * 0.5,
-                ),
-                child: Image.asset('assets/images/logo.png')
-                //child: Image.network("https://upload.wikimedia.org/wikipedia/en/0/05/Indian_Institute_of_Information_Technology%2C_Sonepat_Logo.png")
-                ),
-            Form(
-              key: _loginKey,
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding:
-                        EdgeInsets.only(top: 10.0, left: 20.0, right: 20.0),
-                    child: TextFormField(
-                      controller: email,
-                      keyboardType: TextInputType.emailAddress,
-                      validator: (value) => emailValidator(value),
-                      decoration: InputDecoration(
-                          //labelText: "E-Mail",
-                          hintText: "student@iiitsonepat.com",
-                          prefixIcon: Icon(Icons.email),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          fillColor: Color(0xFF2E313D),
-                          filled: true),
-                      style: TextStyle(
-                        backgroundColor: Color(0xFF2E313D),
-                        // ignore: deprecated_member_use
-                        color: Theme.of(context).textTheme.body2.color,
-                        decorationColor: Color(0xFF2e313d),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Container(
+          child: ListView(
+            children: <Widget>[
+              Container(
+                  constraints: BoxConstraints.expand(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height * 0.5,
+                  ),
+                  child: Image.asset('assets/images/logo.png')
+                  //child: Image.network("https://upload.wikimedia.org/wikipedia/en/0/05/Indian_Institute_of_Information_Technology%2C_Sonepat_Logo.png")
+                  ),
+              Form(
+                key: _loginKey,
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding:
+                          EdgeInsets.only(top: 10.0, left: 20.0, right: 20.0),
+                      child: TextFormField(
+                        controller: email,
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value) => emailValidator(value),
+                        decoration: InputDecoration(
+                            //labelText: "E-Mail",
+                            hintText: "student@iiitsonepat.com",
+                            prefixIcon: Icon(Icons.email),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            fillColor: Color(0xFF2E313D),
+                            filled: true),
+                        style: TextStyle(
+                          backgroundColor: Color(0xFF2E313D),
+                          // ignore: deprecated_member_use
+                          color: Theme.of(context).textTheme.body2.color,
+                          decorationColor: Color(0xFF2e313d),
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding:
-                        EdgeInsets.only(top: 10.0, left: 20.0, right: 20.0),
-                    child: TextFormField(
-                      controller: pass,
-                      obscureText: true,
-                      validator: (value) => pwdValidator(value),
-                      decoration: InputDecoration(
-                          //labelText: "Password",
-                          hintText: "Password",
-                          prefixIcon: Icon(Icons.arrow_forward_ios),
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0)),
-                          fillColor: Color(0xFF2e313d),
-                          filled: true),
-                      style: TextStyle(
-                        backgroundColor: Color(0xFF2E313D),
-                        // ignore: deprecated_member_use
-                        color: Theme.of(context).textTheme.body2.color,
-                        decorationColor: Color(0xFF2e313d),
+                    Padding(
+                      padding:
+                          EdgeInsets.only(top: 10.0, left: 20.0, right: 20.0),
+                      child: TextFormField(
+                        controller: pass,
+                        obscureText: true,
+                        validator: (value) => pwdValidator(value),
+                        decoration: InputDecoration(
+                            //labelText: "Password",
+                            hintText: "Password",
+                            prefixIcon: Icon(Icons.arrow_forward_ios),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0)),
+                            fillColor: Color(0xFF2e313d),
+                            filled: true),
+                        style: TextStyle(
+                          backgroundColor: Color(0xFF2E313D),
+                          // ignore: deprecated_member_use
+                          color: Theme.of(context).textTheme.body2.color,
+                          decorationColor: Color(0xFF2e313d),
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(75, 20, 75, 0),
-              child: RaisedButton(
-                onPressed: () {
-                  if (_loginKey.currentState.validate()) {
-                    print("success");
-                    login();
-                  }
-                },
-                child: Text("Login",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    )),
-                color: Color(0xFFf7c82a),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0)),
+              Padding(
+                padding: EdgeInsets.fromLTRB(75, 20, 75, 0),
+                child: RaisedButton(
+                  onPressed: () {
+                    setState(() {
+                      showSpinner = true;
+                    });
+                    if (_loginKey.currentState.validate()) {
+                      print("success");
+                      login();
+                    }
+                    setState(() {
+                      showSpinner = false;
+                    });
+                  },
+                  child: Text("Login",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      )),
+                  color: Color(0xFFf7c82a),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0)),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
