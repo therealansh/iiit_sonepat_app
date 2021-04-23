3a9a99fea0de4af60ff1ef5c8504f39d4be2a189
ravisehrawat <ravisehrawat625@gmail.com>
2021-04-23 15:26:49 +0530

    Updated backdrop.dart
    Linked the logout button

--- lib/ui/backdrop.dart
+++ lib/ui/backdrop.dart
@@ -5,7 +5,9 @@ import 'package:flutter/rendering.dart';
 import 'package:backdrop/app_bar.dart';
 
 import 'package:backdrop/scaffold.dart';
+import 'package:iiit_sonepat_stable/constants/routes.dart';
 import 'package:iiit_sonepat_stable/ui/About/about.dart';
+import 'package:iiit_sonepat_stable/ui/Auth/SignUp.dart';
 import 'package:iiit_sonepat_stable/ui/DevPage/dev.dart';
 import 'package:iiit_sonepat_stable/ui/DirectorMsg/directormsg.dart';
 import 'package:iiit_sonepat_stable/ui/Home/homepage.dart';
@@ -94,7 +96,7 @@ class _BackdropHome extends State<BackdropHome> {
               ),
             ),
             leading: Icon(Icons.exit_to_app),
-            onTap: () {},
+            onTap: () => {Navigator.pop(context)},
           ),
           Padding(
             padding: EdgeInsets.only(top: 100.0),
