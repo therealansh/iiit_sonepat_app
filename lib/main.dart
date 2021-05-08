e9df38040c0f09f5b5e14ce4c2e57f7b78362ec3
Sanskar Modi <modisanskar5@gmail.com>
2021-05-08 20:28:38 +0530

    Made few changes in naming and state.

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,6 @@ import 'package:iiit_sonepat_stable/ui/Login_Page/Landing.dart';
 import 'package:iiit_sonepat_stable/ui/Login_Page/SplashScreen.dart';
 
 void main() async {
-  Function duringSplash = () {
-    print('Something background process');
-    int a = 123 + 23;
-    print(a);
-
-    if (a > 100)
-      return 1;
-    else
-      return 2;
-  };
-
   Map<int, Widget> op = {1: Landing(), 2: Landing()};
   // WidgetsFlutterBinding.ensureInitialized();
   // await Firebase.initializeApp();
@@ -26,11 +15,9 @@ void main() async {
       home: CustomSplash(
         imagePath: 'assets/images/IIIT Sonepat Logo.png',
         backGroundColor: Colors.white,
-        // backGroundColor: Color(0xfffc6042),
         animationEffect: 'zoom-in',
         logoSize: 200,
         home: Landing(),
-        customFunction: duringSplash,
         duration: 4000,
         type: CustomSplashType.StaticDuration,
         outputAndHome: op,
