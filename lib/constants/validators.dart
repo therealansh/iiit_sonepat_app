e58122fd1bb949230f558071a656f29323716808
VanshKela <75879961+VanshKela@users.noreply.github.com>
2021-06-08 20:04:07 +0530

    Implemented Validator (#47)
    * Implemented Validator
    
    * Validator added with correct regex
    
    * Validation regex changed
    
    * Validation regex changed again and added autoValidateMode
    
    * snackbar replaced with toast

--- lib/constants/validators.dart
+++ lib/constants/validators.dart
@@ -1,18 +1,18 @@
-String pwdValidator(String value) {
+Function(String) pwdValidator = (String value) {
   if (value.length < 8) {
     return 'Password must be longer than 8 characters';
   } else {
     return null;
   }
-}
+};
 
-String emailValidator(String value) {
+Function(String) emailValidator = (String value) {
   Pattern pattern =
-      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+      r'^[a-z]+\.([0-9]{8})+(@iiitsonepat.ac.in)$';
   RegExp regex = new RegExp(pattern);
   if (!regex.hasMatch(value)) {
     return 'Email format is invalid';
   } else {
     return null;
   }
-}
+};
